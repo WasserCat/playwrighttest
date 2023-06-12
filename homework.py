bf439161edd729875973f975b1c5d455e9cbf65c
WasserCat <55379839+WasserCat@users.noreply.github.com>
2023-06-12 15:58:05 +0200

    Test case: Test01

---
+++ homework.py
@@ -0,0 +1,79 @@
+from playwright.sync_api import sync_playwright
+
+
+
+INVENTORY_PAGE_URL = 'https://www.saucedemo.com/inventory.html'
+
+def test_path():
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
+        page = browser.new_page()
+        page.goto('https://www.saucedemo.com')
+
+        username_selector = '#user-name'
+        password_selector = '#password'
+
+        username_field = page.wait_for_selector(username_selector)
+        username_field.fill('standard_user')
+
+        password_field = page.wait_for_selector(password_selector)
+        password_field.fill('secret_sauce')
+
+        button_selector = '#login-button'
+
+        button = page.wait_for_selector(button_selector)
+        button.click()
+
+        button_id = 'react-burger-menu-btn'
+
+        button = page.wait_for_selector(f'button#{button_id}')
+        button.click()
+
+        backpack_button_id = 'add-to-cart-sauce-labs-backpack'
+        bike_light_button_id = 'add-to-cart-sauce-labs-bike-light'
+
+        backpack_button = page.wait_for_selector(f'button#{backpack_button_id}')
+        backpack_button.click()
+
+        bike_light_button = page.wait_for_selector(f'button#{bike_light_button_id}')
+        bike_light_button.click()
+
+        shopping_cart_container = page.wait_for_selector('#shopping_cart_container')
+        shopping_cart_container.click()
+
+        checkout_button_selector = '.btn.btn_action.btn_medium.checkout_button'
+        checkout_button = page.wait_for_selector(checkout_button_selector)
+        checkout_button.click()
+
+        first_name_field = page.wait_for_selector('#first-name')
+        first_name_field.fill('Pawel')
+
+        last_name_field = page.wait_for_selector('#last-name')
+        last_name_field.fill('Gorski')
+
+        postal_code_field = page.wait_for_selector('#postal-code')
+        postal_code_field.fill('999')
+
+        continue_button = page.wait_for_selector('#continue')
+        continue_button.click()
+
+        finish_button = page.wait_for_selector('#finish')
+        finish_button.click()
+
+        back_to_products_button = page.wait_for_selector('#back-to-products')
+        back_to_products_button.click()
+
+
+        current_url = page.url
+
+#        if current_url == inventory_page_url:
+#            print("You are on the inventory page.")
+#        else:
+#            print("You are not on the inventory page.")
+
+#        time.sleep(3)
+        browser.close()
+
+        assert current_url == INVENTORY_PAGE_URL
+
+
