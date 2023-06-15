0d26e57a7a0cda0d73594545857fdc1675eea2f1
Pawel Gorski <pgorskigdansk@gmail.com>
2023-06-15 13:29:30 +0200

    test_path objects file added having class for login page fields and button. Adding file for new test for login

---
+++ test_path_login.py
@@ -0,0 +1,24 @@
+# Test for correct login and password using page object pattern
+
+from playwright.sync_api import sync_playwright
+from buttons import LOGIN_BUTTON_SELECTOR, SHOPPING_CART_BUTTON, CHECKOUT_BUTTON, FINISH_STEP_TWO, BACK_HOME, BACKPACK_BUTTON_SELECTOR, BIKE_LIGHT_BUTTON_SELECTOR
+from test_path_objects import LoginPage
+
+def test_path():
+    with sync_playwright() as playwright:
+        #it's on headless false because I wanted to see what is happening. Normally it would not be on
+        browser = playwright.chromium.launch(headless=False)
+        page = browser.new_page()
+        page.goto('https://www.saucedemo.com')
+
+        assert page.url == 'https://www.saucedemo.com/'
+
+        login_page = LoginPage(page)
+        login_page.login('standard_user', 'secret_sauce')
+
+        # Assertion to check if the login is successful
+        assert page.url == 'https://www.saucedemo.com/inventory.html'
+
+        browser.close()
+def test_empty():
+    pass
---
+++ test_path_objects.py
@@ -0,0 +1,16 @@
+
+
+from buttons import LOGIN_BUTTON_SELECTOR
+
+class LoginPage:
+    def __init__(self, page):
+        self.page = page
+
+    def login(self, username, password):
+        username_field = self.page.wait_for_selector('#user-name')
+        username_field.fill(username)
+
+        password_field = self.page.wait_for_selector('#password')
+        password_field.fill(password)
+
+        self.page.wait_for_selector(LOGIN_BUTTON_SELECTOR).click()
