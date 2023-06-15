b8fbeade41a5976e89a314bdbb218b7666162f56
Pawel Gorski <pgorskigdansk@gmail.com>
2023-06-15 13:55:23 +0200

    Revert "test_path objects file added having class for login page fields and button. Adding file for new test for login"
    This reverts commit ae8ea5981ce20abff3e9205ce588e4d31dbe9744.

--- test_path_login.py
+++ test_path_login.py
@@ -20,21 +20,5 @@ def test_path():
         assert page.url == 'https://www.saucedemo.com/inventory.html'
 
         browser.close()
-
-#test for correct login incorrect passwor
-def test_failed_login():
-    with sync_playwright() as playwright:
-        # it's on headless false because I wanted to see what is happening. Normally it would not be on
-        browser = playwright.chromium.launch(headless=False)
-        page = browser.new_page()
-        page.goto('https://www.saucedemo.com')
-
-        assert page.url == 'https://www.saucedemo.com/'
-
-        login_page = LoginPage(page)
-        login_page.login('standard_user', 'incorrect_password')
-
-        # Assertion to check if the login fails and user remains on the login page
-        assert page.url == 'https://www.saucedemo.com/'
-
-        browser.close()
+def test_empty():
+    pass
